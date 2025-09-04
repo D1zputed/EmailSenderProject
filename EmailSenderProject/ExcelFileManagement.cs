15e37f70d739af2062dd78f42dca4c03d408ddac
D1zputed <asaphdelcoro987@gmail.com>
2025-09-04 20:42:17 +0800

    wired the progress bar to the email sending process

--- EmailSenderProject/ExcelFileManagement.cs
+++ EmailSenderProject/ExcelFileManagement.cs
@@ -11,16 +11,24 @@ namespace EmailSenderProject
     {
         public static void packageExcelFiles(string filePath, ProgressBar progressBar, Label label)
         {
+            progressBar.Visible = true;
+            label.Visible = true;
+
+
             //load the chosen excel file
             XLWorkbook wb = new XLWorkbook(filePath);
+
 
             //query the employee table
             List<Dictionary<string, string>> employees = SQLEngineManagement.RunSelectQuery();
             List<string> errorList = [];
+
+
             // Initialize progress bar
             progressBar.Minimum = 0;
             progressBar.Maximum = employees.Count;
             progressBar.Value = 0;
+
 
             //loop through each employee and take their name and put them as arguments in the method
             foreach (Dictionary<string, string> employee in employees)
@@ -28,6 +36,7 @@ namespace EmailSenderProject
                 XLWorkbook newBook = new XLWorkbook();
                 IXLWorksheet wsSource;
                 string employeeNameInSheet = $"{employee["Last_Name"]}_{employee["First_Name"]}";
+                label.Text = $"Sending Email To: {employeeNameInSheet}";
                 try
                 {
                     wsSource = wb.Worksheet(employeeNameInSheet);
@@ -38,9 +47,11 @@ namespace EmailSenderProject
                     continue;
                 }
 
+
                 wsSource.CopyTo(newBook, employeeNameInSheet);
                 newBook.SaveAs($"{employeeNameInSheet}.xlsx");
                 EmailManagement.sendEmail(employee["Email"], $"{employeeNameInSheet}.xlsx");
+
 
                 // Update progress bar safely on UI thread
                 if (progressBar.InvokeRequired)
@@ -57,6 +68,8 @@ namespace EmailSenderProject
                 string errorMessages = string.Join(Environment.NewLine, errorList);
                 MessageBox.Show(errorMessages, "Sending Unsuccessful for these", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            progressBar.Visible = false;
+            label.Visible = false;
         }
     }
 }
